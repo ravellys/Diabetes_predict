24146bd7b10d30f7c80adc2f0e8521818732db0b
ravellys <lucas.ravellys@ufpe.br>
2020-11-14 16:42:20 -0300

    finalizado

--- modelo_ml/modelo.py
+++ modelo_ml/modelo.py
@@ -4,7 +4,6 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score
 
 
 def pre_tratamento_dados(df, feature_saida):
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import joblib
+from flask import Flask, render_template, request
+import numpy as np
 
 app = Flask(__name__)
 
@@ -7,5 +9,36 @@ def home():
     return render_template("home.html")
 
 
+def previsao_diabetes(lista_formulario):
+    prever = np.array(lista_formulario).reshape(1, -1)
+    modelo_norm = joblib.load('modelo_ml/data_normaliza.sav')
+    prever_norm = modelo_norm.transform(prever)
+    modelo_ml_salvo = joblib.load('modelo_ml/melhor_modelo_ml.sav')
+    resultado = modelo_ml_salvo.predict(prever_norm)
+    return resultado[0]
+
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    if request.method == 'POST':
+        print(request)
+        lista_formulario = request.form.to_dict()
+        print(lista_formulario)
+        lista_formulario = list(lista_formulario.values())
+        lista_formulario = list(map(float, lista_formulario))
+
+        resultado = previsao_diabetes(lista_formulario)
+
+        print(lista_formulario)
+
+        if int(resultado) == 1:
+            previsao = 'Possui diabetes'
+        else:
+            previsao = 'Não Possui diabetes'
+
+        return render_template('result.html', previsao=previsao)
+
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
